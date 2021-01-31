b6859c94b1021a07ff361d73574622090c99e937
Michał Lisowiec <miclisowiec@gmail.com>
2021-01-31 20:30:10 +0100

    Add function setBombs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 'use strict'
 const btnStart = document.querySelector('.start');
 const container = document.querySelector('.container');
+const bombIcon = '<i class="fas fa-bomb"></i>';
+const bombsArray = [];
+//const randomNumer = [];
+const amountOfBombs = 10;
 
 function createGameboard(){
   
@@ -12,3 +16,19 @@ function createGameboard(){
 }
 
 createGameboard();
+
+
+function setBombs(){
+  const cells = document.querySelectorAll('.cell');
+
+  while(bombsArray.length < amountOfBombs){
+    const r = Math.floor(Math.random() * cells.length);
+    if(!bombsArray.includes(cells[r])){
+      bombsArray.push(cells[r]);
+    }
+  }
+
+  bombsArray.forEach(bomb => bomb.innerHTML = bombIcon);
+}
+
+setBombs();
