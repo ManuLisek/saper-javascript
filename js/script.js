21ace08b4817e42909ec9d931835cf5eaa963d0a
Michał Lisowiec <miclisowiec@gmail.com>
2021-02-03 18:02:16 +0100

    Add '1' to all cells adjacent to the bomb

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ const btnStart = document.querySelector('.start');
 const container = document.querySelector('.container');
 const bombIcon = '<i class="fas fa-bomb"></i>';
 const bombsArray = [];
-//const randomNumer = [];
 const amountOfBombs = 10;
+const cellsInRow = 10;
 
 function createGameboard(){
   
@@ -29,6 +29,42 @@ function setBombs(){
   }
 
   bombsArray.forEach(bomb => bomb.innerHTML = bombIcon);
+  const indexesOfBombs = [...cells].map((e, i) => e.innerHTML !== '' ? i : '').filter(Number);
+  indexesOfBombs.map(indexOfBomb => {
+    
+    //right
+    if((indexOfBomb + 1)%10 !== 0 && cells[indexOfBomb + 1].innerHTML !== bombIcon){
+      cells[indexOfBomb + 1].textContent = '1';
+    }
+    //left
+    if(indexOfBomb % 10 !== 0 && cells[indexOfBomb - 1].innerHTML !== bombIcon){
+      cells[indexOfBomb - 1].textContent = '1';
+    }
+    //up
+    if(indexOfBomb - cellsInRow >= 0 && cells[indexOfBomb - cellsInRow].innerHTML !== bombIcon){
+      cells[indexOfBomb - cellsInRow].textContent = '1';
+    }
+    //down
+    if(indexOfBomb + cellsInRow <= cells.length  && cells[indexOfBomb + cellsInRow].innerHTML !== bombIcon){
+      cells[indexOfBomb + cellsInRow].textContent = '1';
+    }
+    //up left
+    if(indexOfBomb % 10 !== 0 && indexOfBomb - cellsInRow >= 0 && cells[indexOfBomb - cellsInRow - 1].innerHTML !== bombIcon){
+      cells[indexOfBomb - cellsInRow - 1].textContent = '1';
+    }
+    //up right
+    if((indexOfBomb + 1)%10 !== 0 && indexOfBomb - cellsInRow >= 0 && cells[indexOfBomb - cellsInRow + 1].innerHTML !== bombIcon){
+      cells[indexOfBomb - cellsInRow + 1].textContent = '1';
+    }
+    //down left
+    if(indexOfBomb % 10 !== 0 && indexOfBomb + cellsInRow -1 <= cells.length && cells[indexOfBomb + cellsInRow - 1].innerHTML !== bombIcon){
+      cells[indexOfBomb + cellsInRow - 1].textContent = '1';
+    }
+    //down right
+    if((indexOfBomb + 1)%10 !== 0 && indexOfBomb + cellsInRow + 1 <= cells.length && cells[indexOfBomb + cellsInRow + 1].innerHTML !== bombIcon){
+      cells[indexOfBomb + cellsInRow + 1].textContent = '1';
+    }
+  })
 }
 
 
