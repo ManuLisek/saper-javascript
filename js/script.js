0a02e86849ea2f484a10ab24e31184aafff04dc8
Michał Lisowiec <miclisowiec@gmail.com>
2021-02-04 19:13:27 +0100

    Put conditions and all cells around bomb in variables

--- js/script.js
+++ js/script.js
@@ -32,39 +32,50 @@ function setBombs(){
   const indexesOfBombs = [...cells].map((e, i) => e.innerHTML !== '' ? i : '').filter(Number.isFinite);
   indexesOfBombs.map(indexOfBomb => {
 
-    
+    const right = cells[indexOfBomb + 1];
+    const left = cells[indexOfBomb - 1];
+    const up = cells[indexOfBomb - cellsInRow];
+    const down = cells[indexOfBomb + cellsInRow];
+    const upLeft = cells[indexOfBomb - cellsInRow - 1];
+    const upRight = cells[indexOfBomb - cellsInRow + 1];
+    const downLeft = cells[indexOfBomb + cellsInRow - 1];
+    const downRight = cells[indexOfBomb + cellsInRow + 1];
+    const rightCondition = (indexOfBomb + 1) % cellsInRow !== 0;
+    const leftCondition = indexOfBomb % cellsInRow !== 0;
+    const upCondition = indexOfBomb - cellsInRow >= 0;
+    const downCondition = indexOfBomb + cellsInRow < cells.length;
     
     //right
-    if((indexOfBomb + 1) % cellsInRow !== 0 && cells[indexOfBomb + 1].innerHTML !== bombIcon){
-      cells[indexOfBomb + 1].textContent = '1';
+    if(rightCondition && right.innerHTML !== bombIcon){
+      right.textContent = '1';
     }
     //left
-    if(indexOfBomb % cellsInRow !== 0 && cells[indexOfBomb - 1].innerHTML !== bombIcon){
-      cells[indexOfBomb - 1].textContent = '1';
+    if(leftCondition && left.innerHTML !== bombIcon){
+      left.textContent = '1';
     }
     //up
-    if(indexOfBomb - cellsInRow >= 0 && cells[indexOfBomb - cellsInRow].innerHTML !== bombIcon){
-      cells[indexOfBomb - cellsInRow].textContent = '1';
+    if(upCondition && up.innerHTML !== bombIcon){
+      up.textContent = '1';
     }
     //down*
-    if(indexOfBomb + cellsInRow < cells.length  && cells[indexOfBomb + cellsInRow].innerHTML !== bombIcon){
-      cells[indexOfBomb + cellsInRow].textContent = '1';
+    if(downCondition  && down.innerHTML !== bombIcon){
+      down.textContent = '1';
     }
     //up left
-    if(indexOfBomb % cellsInRow !== 0 && indexOfBomb - cellsInRow >= 0 && cells[indexOfBomb - cellsInRow - 1].innerHTML !== bombIcon){
-      cells[indexOfBomb - cellsInRow - 1].textContent = '1';
+    if(leftCondition && upCondition && upLeft.innerHTML !== bombIcon){
+      upLeft.textContent = '1';
     }
     //up right
-    if((indexOfBomb + 1) % cellsInRow !== 0 && indexOfBomb - cellsInRow >= 0 && cells[indexOfBomb - cellsInRow + 1].innerHTML !== bombIcon){
-      cells[indexOfBomb - cellsInRow + 1].textContent = '1';
+    if(rightCondition && upCondition && upRight.innerHTML !== bombIcon){
+      upRight.textContent = '1';
     }
     //down left*
-    if(indexOfBomb % cellsInRow !== 0 && indexOfBomb + cellsInRow -1 < cells.length && cells[indexOfBomb + cellsInRow - 1].innerHTML !== bombIcon){
+    if(leftCondition && downCondition && downLeft.innerHTML !== bombIcon){
       cells[indexOfBomb + cellsInRow - 1].textContent = '1';
     }
     //down right
-    if((indexOfBomb + 1) % cellsInRow !== 0 && indexOfBomb + cellsInRow + 1 < cells.length && cells[indexOfBomb + cellsInRow + 1].innerHTML !== bombIcon){
-      cells[indexOfBomb + cellsInRow + 1].textContent = '1';
+    if(rightCondition && downCondition && downRight.innerHTML !== bombIcon){
+      downRight.textContent = '1';
     }
   })
 }
